c47d8dc9eb2b2bacdd5bec038e35bc6ed1ac347b
Yuchen Nie <ynie@salesforce.com>
2026-02-27 09:51:23 -0700

    Added in initial project

---
+++ StravaProfileViewer/StravaProfileViewer/StravaProfileViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravaProfileViewerApp.swift
+//  StravaProfileViewer
+//
+//  Created by Yuchen Nie on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct StravaProfileViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
